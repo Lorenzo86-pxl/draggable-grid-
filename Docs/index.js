4b3ec02b0da4cfff0004b294e6958c200addb23f
Lorenz Löffler <2010738210@stud.fh-kufstein.ac.at>
2021-05-12 15:29:38 +0200

    update

--- Docs/index.js
+++ Docs/index.js
@@ -1,15 +1,17 @@
-function swapItems(index1, index2) 
+/*function swapItems(index1, index2) 
 {
     const element1 = document.querySelector("[data-index='" + index1 + "']");
     const element2 = document.querySelector("[data-index='" + index2 + "']");
     let sibling1 = element1.previousElementSibling;
     let sibling2 = element2.previousElementSibling;
+    //Sibling als Geschwisterelemnt verwendet
    
     if (!sibling1) 
     {
         const parent = element1.parentElement;
         element2.insertAdjacentElement('afterend', element1);
         parent.insertAdjacentElement('afterbegin', element2);
+        //Eltern Verzeichnis
 
     } else if (!sibling2) 
     {
@@ -29,14 +31,52 @@ function swapItems(index1, index2)
         {
             element1.insertAdjacentElement('afterend', element2);
             sibling2.insertAdjacentElement('afterend', element1);
+            //Verbindung
         }
     }
-}
+}*/
+
+/**
+*
+* index1 refers to the element which is dragged
+* index2 refers to the element which is the drop-target
+*
+*/
+function swapItems(index1, index2) {
+    const element1 = document.querySelector(`[data-index="${index1}"]`);
+    const element2 = document.querySelector(`[data-index="${index2}"]`);
+    
+    
+    
+    if (element1 != element2) {
+    const draggableItemsContainer = element1.parentElement;
+    
+    if (!element1.nextElementSibling) {
+    element2.insertAdjacentElement('beforebegin', element1);
+    draggableItemsContainer.removeChild(element2);
+    draggableItemsContainer.appendChild(element2);
+    } else if (element1.nextElementSibling == element2) {
+    draggableItemsContainer.removeChild(element1);
+    element2.insertAdjacentElement('afterend', element1);
+    } else {
+    const next = element1.nextElementSibling;
+    draggableItemsContainer.removeChild(element1);
+    element2.insertAdjacentElement('beforebegin', element1);
+    draggableItemsContainer.removeChild(element2);
+    next.insertAdjacentElement('beforebegin', element2);
+    }
+    }
+    
+    
+    
+    element2.classList.remove('dragover');
+    }
+    
+    
 
 function initDragAndDrop() 
 {
     const draggableItemsContainer = document.querySelector('ul');
-
     draggableItemsContainer.addEventListener('dragstart', (e) => 
     {
         e.target.classList.add('dragged');
@@ -80,6 +120,7 @@ function initDragAndDrop()
         e.target.classList.remove('dragover');
         const index1 = e.dataTransfer.getData('text/plain');
         const index2 = e.target.dataset.index;
+        // TODO: HTML-Elemente tauschen
        
         if (index1 && index2) 
         {
@@ -96,7 +137,7 @@ function initTouch()
     let lastX = 0;
     let lastY = 0;
 
-    for(i=0; i<draggableItems.length;i++){
+    for(let i=0; i<draggableItems.length;i++){
         draggableItems[i].addEventListener('touchstart', (e) => 
         {
             initialX = e.touches[0].clientX;
@@ -121,7 +162,8 @@ function initTouch()
             } else 
             {
                 let list = document.getElementsByClassName('dragover');
-                if(list.length > 0){
+                if(list.length > 0)
+                {
                     list[0].classList.remove('dragover');
                 }
             }
@@ -130,7 +172,8 @@ function initTouch()
         draggableItems[i].addEventListener('touchend', (e) => 
         {
             const elementList = document.elementsFromPoint(lastX, lastY);
-            if (elementList.length > 1 && elementList[1].hasAttribute('draggable')) 
+            if (elementList.length > 1 && elementList[1].hasAttribute('draggable'))
+            // die swapItems Funktion wurde bereits in Aufgabe 1b von Ihnen erstellt 
             {
                 swapItems(e.target.dataset.index, elementList[1].dataset.index);
             }
